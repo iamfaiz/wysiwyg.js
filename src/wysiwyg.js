88d8bdd93c5b72ac51dac746658cbff2626858be
unknown <iamfaiz@outlook.com>
2014-08-29 10:43:38 +0300

    Basics Done

--- src/wysiwyg.js
+++ src/wysiwyg.js
@@ -2,13 +2,94 @@ $(window).load(function(){
 
 	$.fn.wysiwyg = function ( options ) {
 
-		var iframe = $("<iframe>");
+		var iframe = $('<iframe class="wysiwyg">');
 
 		$(this).replaceWith(iframe);
 
 		iframe[0].contentDocument.body.innerHTML = $(this).text();
 
+		iframe[0].contentDocument.designMode = "On";
 
+		if ( iframe[0].contentWindow )
+		{
+
+			iframe[0].contentWindow.document.open();
+			// optionally write content here
+			iframe[0].contentWindow.document.close();
+			iframe[0].contentWindow.document.designMode = "on";
+			iframe[0].contentWindow.document.body.innerHTML = $(this).text();
+
+		}
+
+		if (options.size.height)
+		{
+			iframe.height(options.size.height);
+		}
+
+		if (options.size.width)
+		{
+			iframe.width(options.size.width);
+		}
+
+		if ( options.controls.bold )
+		{
+			options.controls.bold.click(function(e){
+				e.preventDefault();
+				iframe[0].contentDocument.execCommand('bold', false, null);
+			});
+		}
+
+		if ( options.controls.italic )
+		{
+			options.controls.italic.click(function(e){
+				e.preventDefault();
+				iframe[0].contentDocument.execCommand('italic', false, null);
+			});
+		}
+
+		if ( options.controls.underline )
+		{
+			options.controls.underline.click(function(e){
+				e.preventDefault();
+				iframe[0].contentDocument.execCommand('underline', false, null);
+			});
+		}
+
+// input controls object
+
+		if ( options.inputControls.fontName )
+		{
+			options.inputControls.fontName.change(function(e){
+				e.preventDefault();
+				var val = options.inputControls.fontName.val();
+				iframe[0].contentDocument.execCommand('fontName', false, val);
+				// console.log(val); 
+			});
+		}
+
+		if ( options.inputControls.link )
+		{
+			options.inputControls.link.change(function(e){
+				e.preventDefault();
+				var val = options.inputControls.link.val();
+				iframe[0].contentDocument.execCommand('createLink', false, val);
+				// console.log(val); 
+			});
+		}
+
+// defaults object
+
+		if ( options.defaults.fontFamily ) 
+		{
+			iframe[0].contentDocument.body.style.fontFamily = options.defaults.fontFamily;
+		}
+
+		if ( options.defaults.border ) 
+		{
+			iframe[0].style.border = options.defaults.border;
+		}
+
+		return iframe[0].contentDocument.body.innerHTML;
 
 	};
 
